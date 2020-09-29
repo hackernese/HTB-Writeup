a7a6922351396b2df1de8adae864787e31177c71
Hackernese <47540875+Zenix-Owler@users.noreply.github.com>
2020-09-29 09:25:27 +0700

    Delete vitalline.py

--- Blunder-10.10.10.191/vitalline.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-
-linenum = 0
-with open(sys.argv[1]) as file_:
-    for line in file_:
-        linenum += 1
-        if "[1;31m" in line:
-            print("Line {} :".format(linenum),line,end="")
